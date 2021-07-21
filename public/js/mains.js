2d2521dfe1d0e2d8a5fba26089bcae88374581db
kagritu <rituparna_ug@cse.nits.ac.in>
2021-07-21 20:46:49 +0530

    new changes added

--- public/js/mains.js
+++ public/js/mains.js
@@ -1,6 +1,6 @@
 //now we want to add the messages we sent in the chat
 const chatForm = document.getElementById('chat-form');
-
+const chatMessages = document.querySelector('.chat-messages');
 
 const socket = io();
 
@@ -9,18 +9,27 @@ socket.on('message', message => {
     console.log(message);
 
     outputMessage(message);
+
+    //scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 //message submit, we are creating an event listener for submission of that form
 chatForm.addEventListener('submit', (e) => {
+    //to stop submission of the form to a file
     e.preventDefault();
 
     //now we need to get the text input
-    const msg= e.target.elements.msg.value;
+    const msg = e.target.elements.msg.value;
     //we have a id="msg" in the chat.html
 
     //emit message to server
      socket.emit('chatMessage', msg);
+
+     //clear input
+     e.target.elements.msg.value = '';
+     e.target.elements.msg.focus();
+
 });
 
 //output message to DOM(DOM manipulation)
--- server.js
+++ server.js
@@ -1,36 +1,40 @@
-const path= require('path');
-const http= require('http');
+const path = require('path');
+const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-
+const formatMessage = require('./utils');
 
 const app = express();
-const server= http.createServer(app);
+//we could create server through express, but we want to use it directly
+const server = http.createServer(app);
 const io = socketio(server);
 
 
 //to set static folder, so that we can use our front end part
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'ChatApp Bot';
+
 //run when client connects
 io.on('connection', socket => {
     //now this will emit the message
     //this message is shown to the single user thats connecting
-    socket.emit('message', 'Welcome to ChatApp!');
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatApp!'));
 
     //broadcast when a user connects
     //this message will shown to everyone except the client that joined the chat
-    socket.broadcast.emit('message', 'a user has joined the chat');
+    socket.broadcast.emit('message', formatMessage(botName,  'A user has joined the chat'));
 
     //Runs when client disconnects
     socket.on('disconnect', () =>{
-        //io.emit as to show this message to evryone
-        io.emit('message', 'A user has left the chat');
+        //io.emit as to show this message to everyone
+        io.emit('message', formatMessage(botName, 'A user has left the chat'));
     });
 
     //listen for chatMessage
     socket.on('chatMessage', (msg) => {
-        io.emit('message', msg);
+        //we want to emit it to everybody, instead of console
+        io.emit('message', formatMessage('USER', msg));
     });
 });
 
